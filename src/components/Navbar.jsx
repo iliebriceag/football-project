e86a940de6412b00e5de85610e229d04b4fb28aa
Teri01 <briceag.ilie97@gmail.com>
2024-08-05 09:45:31 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import './../css/Navbar.css';
 
 function Navbar() {
+    const navbarCollapseRef = useRef(null);
+
+    const handleLinkClick = () => {
+        if (navbarCollapseRef.current) {
+            navbarCollapseRef.current.classList.remove('show');
+        }
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark mb-5">
-            <div class="container">
-                <img src="/icon.png" class="logo" alt="fotabl"></img><Link class="navbar-brand fw-bold ms-1" to="/">Fotbal</Link>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <div className="container">
+                <img src="/icon.png" className="logo" alt="fotabl" />
+                <Link className="navbar-brand fw-bold ms-1" to="/">Fotbal</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-center">
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/">Acasă</Link>
+                <div className="collapse navbar-collapse" id="navbarNav" ref={navbarCollapseRef}>
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 text-center">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/" onClick={handleLinkClick}>Acasă</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/categorii">Categorii</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/categorii" onClick={handleLinkClick}>Categorii</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/contact">Contact</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/contact" onClick={handleLinkClick}>Contact</Link>
                         </li>
-
                     </ul>
                 </div>
             </div>
