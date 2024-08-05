2eae80cbd4d2d99a2ea03c841486f9e192be5936
Teri01 <briceag.ilie97@gmail.com>
2024-08-05 14:34:47 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/Main.css';
+import './css/main.css';
 import './css/Animations.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
